b11dcb434d3cacfffdbf95bb236e76e77562a208
cornflow <marko.corn@gmail.com>
2023-01-24 15:03:12 +0100

    added display server running on port

--- app.js
+++ app.js
@@ -8,4 +8,6 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(8080);
+app.listen(8080, () => {
+    console.log('Server is up on port!'+8080);
+});
